40f604d54c2eae68d2f84dc32aa27aafdcb33795
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-09-22 23:43:34 +0900

    docs : update 9.22.sql

--- SQL 방과후/9.22. Division(디비전) 1번문제.sql
+++ SQL 방과후/9.22. Division(디비전) 1번문제.sql
@@ -1,3 +1,20 @@
 SELECT 교과목번호 FROM 이수
  GROUP BY 교과목번호
- HAVING (SELECT count(*) from 학생) = count(학번)
+ HAVING (SELECT count(*) from 학생) = count(학번);
+
+ -- NOT EXISTS * 2
+SELECT 교과목명 FROM 교과목
+ WHERE NOT EXISTS (
+	SELECT 학생.학번 FROM 학생
+	 WHERE NOT EXISTS ( 
+		 SELECT 이수.학번 FROM 이수
+		  WHERE 교과목.교과목번호 = 이수.교과목번호
+            AND 학생.학번 = 이수.학번));
+
+-- NOT EXISTS, NOT IN
+SELECT 교과목명 FROM 교과목
+ WHERE NOT EXISTS (
+	SELECT 학생.학번 FROM 학생
+	 WHERE 학번 NOT IN (
+		SELECT 학번 FROM 이수
+		 WHERE 이수.교과목번호 = 교과목.교과목번호));
--- SQL 방과후/9.22. Division(디비전) 2번문제.sql
+++ SQL 방과후/9.22. Division(디비전) 2번문제.sql
@@ -1,5 +1,25 @@
-SELECT 이수.교과목번호 FROM 이수 
-  JOIN 교과목 ON 이수.교과목번호 = 교과목.교과목번호
- WHERE 학과명 = "소프트웨어개발과"
- GROUP BY 이수.교과목번호
-HAVING (SELECT count(*) from 학생) = count(학번)
+SELECT 성명 FROM 학생 JOIN (SELECT 학번 FROM 이수 JOIN 교과목 
+							  ON 이수.교과목번호 = 교과목.교과목번호
+						   WHERE 학과명 = "소프트웨어개발과"
+						   GROUP BY 학번
+						  HAVING count(*) = (SELECT count(*) from 교과목 WHERE 학과명 = "소프트웨어개발과")
+                          ) AS 뷰 ON 뷰.학번 = 학생.학번; 
+
+ -- NOT EXISTS * 2
+SELECT 성명 FROM 학생
+ WHERE NOT EXISTS (
+	SELECT * FROM 교과목 A 
+     WHERE 학과명 = "소프트웨어개발과" 
+	   AND NOT EXISTS (
+			SELECT * FROM 이수 B 
+             WHERE A.교과목번호 = B.교과목번호
+               AND B.학번 = 학생.학번));
+
+-- NOT EXISTS, NOT IN
+SELECT 성명 FROM 학생
+ WHERE NOT EXISTS (
+	SELECT * FROM 교과목
+	 WHERE 학과명 = "소프트웨어개발과" 
+       AND 교과목번호 NOT IN (
+		SELECT 교과목번호 FROM 이수
+		 WHERE 학생.학번 = 이수.학번));
