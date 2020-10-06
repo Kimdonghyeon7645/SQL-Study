31c1deccdf4eca632cae0a7564e228c22d31f2dd
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-10-06 18:19:45 +0900

    docs : update 10.6. 조건문.sql

--- SQL 방과후/10.6. 조건문.sql
+++ SQL 방과후/10.6. 조건문.sql
@@ -1,10 +1,11 @@
 use sql_study;
 
 -- 학번이 짝수면 짝수, 홀수면 홀수 출력하는 쿼리문
-# if(조건문, 참일때 값, 거짓일때 값)
+# if(조건문, 참일때 값, 거짓일때 값) : if 문
 select 학번, if(학번 % 2 = 0, '짝수', '홀수') as 결과 from 이수;
 
 -- 기말 성적에 따라서 A~E를 출력하는 쿼리문
+# CASE 조건 or 값 when 값1 then 표시값 when 값2 then 표시값 ... else 표시값 END : switch 문 
 select 기말성적,
 		CASE truncate(기말성적 / 10, 0)
 			WHEN 10 THEN 'A'
