938073c00da38f631fdd24ce689c0759131aed71
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-10-07 22:30:57 +0900

    docs : update 10.6. 통계.sql

--- SQL 방과후/10.6. 조건문.sql
+++ SQL 방과후/10.6. 조건문.sql
@@ -4,6 +4,7 @@ use sql_study;
 # if(조건문, 참일때 값, 거짓일때 값) : if 문
 select 학번, if(학번 % 2 = 0, '짝수', '홀수') as 결과 from 이수;
 
+
 -- 기말 성적에 따라서 A~E를 출력하는 쿼리문
 # CASE 조건 or 값 when 값1 then 표시값 when 값2 then 표시값 ... else 표시값 END : switch 문 
 select 기말성적,
@@ -13,5 +14,15 @@ select 기말성적,
 			WHEN 8 THEN 'B'
 			WHEN 7 THEN 'C'
 			WHEN 6 THEN 'D'
+			ELSE 'E'
+		END as 결과 from 이수;
+
+# CASE when 조건식1 then 표시값 when 조건식2 then 표시값 ... else 표시값 END : if, elif 문 
+select 기말성적,
+		CASE 
+			WHEN truncate(기말성적, 0) >= 90 THEN 'A'
+			WHEN truncate(기말성적, 0) >= 80 THEN 'B'
+			WHEN truncate(기말성적, 0) >= 70 THEN 'C'
+			WHEN truncate(기말성적, 0) >= 60 THEN 'D'
 			ELSE 'E'
 		END as 결과 from 이수;
---
+++ SQL 방과후/10.6. 통계.sql
@@ -0,0 +1,10 @@
+use sql_study;
+
+-- 중간성적과 기말성적의 평균을 5등급으로 나눠, 1~5등급으로 출력하는 쿼리문
+# ntile(등급 개수) over (order by 정렬조건) : 정렬 조건에 따라 등급 개수만큼 나눔
+select 중간성적, 기말성적, (중간성적+기말성적)/2 as 중간기말평균, ntile(5) over (order by (중간성적+기말성적)/2 desc) as 등급 from 이수;
+ 
+
+-- 학생별로 이수한 과목 중간성적, 기말성적 합계, 마지막 행에 전체 학생의 중간성적, 기말성적 합계 추가해 출력하는 쿼리문
+# rollup :
+select 학번, sum(중간성적), sum(기말성적) from 이수 group by 학번 with rollup;
