abeb9185bcd798a4fc807b749f7b67f2891292a0
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-11-17 17:07:04 +0900

    docs : add 11.17. 연습문제.sql

---
+++ SQL 방과후/11.17. 연습문제.sql
@@ -0,0 +1,22 @@
+use sql_study;
+
+alter table 교수 add column 순위 int;
+
+insert into 교수 values
+	(5, "롸이겔루스", 5);
+
+update 교수 set 순위 = 2
+ where name = "가네";
+-- safe update 로 에러가 난다면, edit > preferences... > SQL Editor > Other 의 Safe Updates를 꺼주고, MySQL Workbench를 재연결하자
+ 
+select count(교수.순위) as count 
+  from 교수 natural join 이론
+ where no > 1;
+# 위는 (1) 실행 구문
+
+-- 공부 릴레이션과 같은 뷰(View)가 생성되도록 SQL구문 작성
+
+
+# 아래는 상태 테스트 구문 
+select * from 교수;
+select * from 교수 natural join 이론
