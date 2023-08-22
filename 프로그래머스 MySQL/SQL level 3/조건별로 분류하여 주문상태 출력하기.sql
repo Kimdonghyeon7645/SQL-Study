dc5671bbba0ca6fd6ed76fba3064a83935deba3f
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2023-08-22 10:25:24 +0900

    code : level 3 문제

---
+++ 프로그래머스 MySQL/SQL level 3/조건별로 분류하여 주문상태 출력하기.sql
@@ -0,0 +1,10 @@
+SELECT ORDER_ID
+     , PRODUCT_ID
+     , DATE_FORMAT(OUT_DATE, '%Y-%m-%d') AS OUT_DATE
+     , CASE
+        WHEN OUT_DATE IS NULL THEN '출고미정'
+        WHEN DATEDIFF(OUT_DATE, "2022-05-01") > 0 THEN '출고대기'
+        WHEN DATEDIFF(OUT_DATE, "2022-05-01") <= 0 THEN '출고완료'
+       END AS '출고여부'
+  FROM FOOD_ORDER
+ ORDER BY ORDER_ID; 
